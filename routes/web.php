66e3fbc0e757d01a9f9cc1bd3488e76671b1ac49
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-02 22:39:10 +0200

    Add logic for editing, updating, and deleting a job listing

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get("/", function () {
     return view("home");
 });
 
+// Index
 Route::get("/jobs", function () {
     // get() -> select *
     // use limiting or pagination for larger number of records
@@ -15,19 +16,21 @@ Route::get("/jobs", function () {
     return view("jobs.index", ["jobs" => $jobs]);
 });
 
+// Create
 Route::get("/jobs/create", function () {
     return view('jobs.create');
 });
 
-Route::get("/jobs/{id}", function ($id) {
-    $job = Job::find($id);
+// Show
+Route::get("/jobs/{job}", function (Job $job) {
     return view("jobs.show", ["job" => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
-        'title'=>['required','min:3'],
-        'salary'=>['required'],
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
     ]);
 
     Job::create([
@@ -36,6 +39,38 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get("/jobs/{job}/edit", function (Job $job) {
+    return view("jobs.edit", ["job" => $job]);
+});
+
+// Update
+Route::patch("/jobs/{job}", function (Job $job) {
+//Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+// Authorize {on hold...}
+
+// update the job
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+// redirect
+    return redirect("/jobs/" . $job->id);
+});
+
+// Destroy
+Route::delete("/jobs/{job}", function (Job $job) {
+// Authorize {on hold...}
+    $job->delete();
     return redirect('/jobs');
 });
 
