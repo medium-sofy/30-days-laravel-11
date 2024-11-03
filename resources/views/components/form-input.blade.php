ea042906fdf4b38fb1dacba5478278a043af478f
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-03 20:16:27 +0200

    Fix form input hidden borders

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,4 +1,4 @@
 <div>
     <input
-        {{ $attributes->merge(['class' => 'block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6']) }}>
+        {{ $attributes->merge(['class' => 'block flex-1 border-2 border-solid border-gray-300 rounded-md bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6']) }}>
 </div>
