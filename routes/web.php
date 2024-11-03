ffc4267574a7b3888ec46976f5bbf5c210c992f1
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-03 18:26:57 +0200

    Add registration endpoints to routes file

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::view('/', 'home');
 Route::view('/', 'contact');
 
 Route::resource('jobs',JobController::class);
 
-
+// Auth
+Route::get('/register', [RegisteredUserController::class,'create']);
+Route::post('/register', [RegisteredUserController::class,'store']);
