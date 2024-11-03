651dfb563e290e9b74c4012254fad7695184632a
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-03 19:43:08 +0200

    Add logic to register a new user to the app

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
 class RegisteredUserController extends Controller
 {
     public function create()
@@ -11,6 +15,18 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+        // create the user
+        $user = User::create($attributes);
+        // log in
+        Auth::login($user);
+        // redirect somewhere
+        return redirect('/jobs');
     }
 }
