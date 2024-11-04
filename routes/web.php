95de89d6f2ffa9839519e1cfe53a89330c9b30d1
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-04 20:35:11 +0200

    Expand Job resource route into individual routes to add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs',JobController::class);
+Route::resource('jobs', JobController::class)->middleware('auth');
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+->middleware('auth')
+// AppServiceProvider: Gate::define('edit-job')
+->can('edit-job', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 // Auth
-Route::get('/register', [RegisteredUserController::class,'create']);
-Route::post('/register', [RegisteredUserController::class,'store']);
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class,'create']);
-Route::post('/login', [SessionController::class,'store']);
-Route::post('/logout', [SessionController::class,'destroy']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
